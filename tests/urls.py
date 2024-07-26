47d4727a9cccd98f5a84f2e70a88ca49fff1b5c1
kmh0601 <pongkmh@naver.com>
2024-07-26 19:48:14 +0900

    [chore] oauth 구현을 위한 임시 api

--- tests/urls.py
+++ tests/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from tests.views import *
 
 urlpatterns = [
-    path('', hello_world_test, name = 'hello_world_test'),
-    path('test/', connections_test, name = "connection_test")
+    path('test/', hello_world_test, name = 'hello_world_test'),
+    path('connect/', connections_test, name = "connection_test"),
+
+    path('list/', PostList.as_view()),
+    path('<int:id>/', PostDetail.as_view())
 ]
