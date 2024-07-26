fff3b7a7b85c9c420c27146eb35e8e34959a796b
kmh0601 <pongkmh@naver.com>
2024-07-26 19:48:45 +0900

    [chore] oauth 구현을 위한 임시 api

--- tests/views.py
+++ tests/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
+# 프/백 연결 테스트 api
 def hello_world_test(request):
     if request.method == "GET":
         return JsonResponse({
@@ -15,3 +16,39 @@ def connections_test(request):
             'status' : 200,
             'data' : 'connection success~'
         })
+
+# 소셜 로그인 구현을 위한 테스트 api
+from .serializers import PostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Post
+
+class PostList(APIView):
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+class PostDetail(APIView):
+    def get(self, request, id):
+        post = get_object_or_404(Post, id=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+class PostDetail(APIView):
+    def delete(self,request,id):
+        post = get_object_or_404(Post, id=id)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
