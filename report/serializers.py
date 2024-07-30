0accf62196baa84eb676021aad600f6068d4fed9
yeahcold <roganyc@naver.com>
2024-07-30 17:01:52 +0900

    [fix] report view 수정 중

--- report/serializers.py
+++ report/serializers.py
@@ -1,17 +1,35 @@
-###Model Serializer case
 from rest_framework import serializers
-from .models import *
-from place.serializers import *
+from .models import Report
+from place.models import *
+from place.serializers import SmokingPlaceSerializer, SecondhandSmokingPlaceSerializer
 
 class ReportSerializer(serializers.ModelSerializer):
-    place = PlaceSerializer()
+    smokingPlace = SmokingPlaceSerializer(required=False)
+    secondhandSmokingPlace = SecondhandSmokingPlaceSerializer(required=False)
 
     class Meta:
         model = Report
-        fields = ['reportId', 'place', 'userId', 'description', 'reportType']
+        fields = ['reportId', 'smokingPlace', 'secondhandSmokingPlace', 'userId', 'description', 'reportType']
+
+    def validate(self, data):
+        if data['reportType'] == 'SM':
+            if 'smokingPlace' not in data:
+                raise serializers.ValidationError("흡연 구역 제보에는 'smokingPlace' 데이터가 필요합니다.")
+        elif data['reportType'] == 'SH':
+            if 'secondhandSmokingPlace' not in data:
+                raise serializers.ValidationError("간접 흡연 구역 제보에는 'secondhandSmokingPlace' 데이터가 필요합니다.")
+        return data
 
     def create(self, validated_data):
-        place_data = validated_data.pop('place')
-        place = Place.objects.create(**place_data)
-        user = validated_data.pop('userId')
-        return Report.objects.create(placeId=place, userId=user, **validated_data)
+        report_type = validated_data.get('reportType')
+
+        if report_type == 'SM':
+            place_data = validated_data.pop('smokingPlace')
+            place = SmokingPlace.objects.create(**place_data)
+            validated_data['smokingPlace'] = place
+        elif report_type == 'SH':
+            place_data = validated_data.pop('secondhandSmokingPlace')
+            place = SecondhandSmokingPlace.objects.create(**place_data)
+            validated_data['secondhandSmokingPlace'] = place
+
+        return Report.objects.create(**validated_data)
