9b8b4c207bb90b7012a64eebedb2dc783ff3121e
kmh0601 <pongkmh@naver.com>
2024-08-01 17:33:30 +0900

    [feat] 배포용 설정 코드

--- config/settings.py
+++ config/settings.py
@@ -112,53 +112,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# ssh 터널링
-
-
-if os.getenv('RUN_MAIN') == 'true':
-    from sshtunnel import SSHTunnelForwarder
-    # SSH 터널 설정
-    SSH_HOST = 'ec2-3-38-250-108.ap-northeast-2.compute.amazonaws.com'
-    SSH_USER = 'ubuntu'
-    SSH_PRIVATE_KEY = '/Users/kwonminhyeok/Desktop/LIKELION_CAU/hackathon/LikeLion12_hackathon.pem'
-    RDS_HOST = 'bbuhackathon.clco8q60opsx.ap-northeast-2.rds.amazonaws.com'
-    RDS_PORT = 3306
-    LOCAL_PORT = 3306  # 로컬 머신에서 사용할 포트
-
-    # SSH 터널 시작
-    server = SSHTunnelForwarder(
-        (SSH_HOST, 22),
-        ssh_username=SSH_USER,
-        ssh_private_key=SSH_PRIVATE_KEY,
-        remote_bind_address=(RDS_HOST, RDS_PORT),
-        local_bind_address=('127.0.0.1', LOCAL_PORT)
-    )
-
-    server.start()
-
-    import atexit
-    atexit.register(server.stop)
-
-# from sshtunnel import SSHTunnelForwarder
-# # SSH 터널 설정
-# SSH_HOST = 'ec2-3-38-250-108.ap-northeast-2.compute.amazonaws.com'
-# SSH_USER = 'ubuntu'
-# SSH_PRIVATE_KEY = '/Users/kwonminhyeok/Desktop/LIKELION_CAU/hackathon/LikeLion12_hackathon.pem'
-# RDS_HOST = 'bbuhackathon.clco8q60opsx.ap-northeast-2.rds.amazonaws.com'
-# RDS_PORT = 3306
-# LOCAL_PORT = 3306  # 로컬 머신에서 사용할 포트
-
-# # SSH 터널 시작
-# server = SSHTunnelForwarder(
-#     (SSH_HOST, 22),
-#     ssh_username=SSH_USER,
-#     ssh_private_key=SSH_PRIVATE_KEY,
-#     remote_bind_address=(RDS_HOST, RDS_PORT),
-#     local_bind_address=('127.0.0.1', LOCAL_PORT)
-# )
-
-# server.start()
-
 
 DATABASES = {
 	'default': {
@@ -166,16 +119,10 @@ DATABASES = {
 		'NAME': get_secret("DB_NAME"),
 		'USER': 'admin', # root로 접속하여 DB를 만들었다면 'root'
 		'PASSWORD': get_secret("DB_PASSWORD"),
-		'HOST': "127.0.0.1",
+		'HOST': get_secret("DB_HOST"),
 		'PORT': '3306',
 	}
 }
-
-# def close_ssh_tunnel():
-#     server.stop()
-
-# import atexit
-# atexit.register(close_ssh_tunnel)
 
 
 # Password validation
@@ -242,6 +189,7 @@ SIMPLE_JWT = {
 
 KAKAO_CONFIG = {
     "KAKAO_REST_API_KEY": get_secret("KAKAO_REST_API_KEY"),
+    # "KAKAO_REDIRECT_URI": "http://localhost:3000/oauth",
     "KAKAO_REDIRECT_URI": "https://wellnesstour.netlify.app/oauth",
     "KAKAO_CLIENT_SECRET_KEY": get_secret("KAKAO_CLIENT_SECRET_KEY"), 
 }
