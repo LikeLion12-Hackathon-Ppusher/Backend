f5b3854a3272f6e5566524f0b7270c0a729132f3
kmh0601 <pongkmh@naver.com>
2024-08-03 01:44:02 +0900

    [feat] place url에 reportSmokingPlace url추가

--- place/urls.py
+++ place/urls.py
@@ -6,6 +6,8 @@ from place.views import *
 urlpatterns = [
     path('smoking/', SmokingPlaceList.as_view()),
     path('smoking/<int:id>/', SmokingPlaceDetail.as_view()),
+    path('reportsmoking/', ReportSmokingPlaceList.as_view()),
+    path('reportsmoking/<int:id>/', ReportSmokingPlaceDetail.as_view()),
     path('nosmoking/', NoSmokingPlaceList.as_view()),
     path('nosmoking/<int:id>/', NoSmokingPlaceDetail.as_view()),
     path('shsmoking/', SecondhandSmokingPlaceList.as_view()),
